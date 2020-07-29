21ca0d1ccb9a1a3f9ee4b025a11550781f2af067
Jordi <jordilucas3@gmail.com>
2020-07-29 09:09:29 -0300

    implementacao bookDetailsActivity

--- app/src/main/java/com/jordilucas/livros/BookDetailsActivity.kt
+++ app/src/main/java/com/jordilucas/livros/BookDetailsActivity.kt
@@ -1,11 +1,37 @@
 package com.jordilucas.livros
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.jordilucas.livros.databinding.ActivityBookDetailsBinding
+import com.jordilucas.livros.model.Book
+import org.parceler.Parcels
 
 class BookDetailsActivity : AppCompatActivity() {
+
+    private val binding:ActivityBookDetailsBinding by lazy{
+        DataBindingUtil.setContentView<ActivityBookDetailsBinding>(
+            this, R.layout.activity_book_details
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_book_details)
+        val book = Parcels.unwrap<Book>(intent.getParcelableExtra(EXTRA_BOOK))
+        if(book!=null){
+            binding.book = book
+        }
     }
+
+    companion object{
+        private const val EXTRA_BOOK = "book"
+        fun start(context: Context, book: Book){
+            Intent(context, BookDetailsActivity::class.java).apply {
+                putExtra(EXTRA_BOOK, Parcels.wrap(book))
+            }
+        }
+    }
+
 }
