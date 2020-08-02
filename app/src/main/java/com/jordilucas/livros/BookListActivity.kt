beb7731ec19f1f2a26b309c30100367658a3261e
Jordi <jordilucas3@gmail.com>
2020-08-02 11:03:07 -0300

    BindingAdapter para load de imagens usando glid

--- app/src/main/java/com/jordilucas/livros/BookListActivity.kt
+++ app/src/main/java/com/jordilucas/livros/BookListActivity.kt
@@ -14,7 +14,7 @@ class BookListActivity : AppCompatActivity() {
             id="1",
             title = "Dominando Android com Kotlin",
             author="Nelson Glauber",
-            coverUrl = "https://s3.novatec.com.br/capas-ampliadas/capas-ampliada-9788575224632.jpg",
+            coverUrl = "https://s3.novatec.com.br/capas-ampliadas/capa-ampliada-9788575224632.jpg",
             pages=954,
             year=2018,
             publisher = Publisher("1", "Novatec"),
