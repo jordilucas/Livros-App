b28f542f25420ab72c4414a53b80224b4139f863
Jordi <jordilucas3@gmail.com>
2020-08-02 10:30:39 -0300

    add startActivity

--- app/src/main/java/com/jordilucas/livros/BookDetailsActivity.kt
+++ app/src/main/java/com/jordilucas/livros/BookDetailsActivity.kt
@@ -5,14 +5,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import com.jordilucas.livros.databinding.ActivityBookDetailsBinding
+import com.jordilucas.livros.databinding.ActivityBookBinding
 import com.jordilucas.livros.model.Book
 import org.parceler.Parcels
 
 class BookDetailsActivity : AppCompatActivity() {
 
-    private val binding:ActivityBookDetailsBinding by lazy{
-        DataBindingUtil.setContentView<ActivityBookDetailsBinding>(
+    private val binding: ActivityBookBinding by lazy {
+        DataBindingUtil.setContentView<ActivityBookBinding>(
             this, R.layout.activity_book_details
         )
     }
@@ -20,17 +20,19 @@ class BookDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val book = Parcels.unwrap<Book>(intent.getParcelableExtra(EXTRA_BOOK))
-        if(book!=null){
+        if (book != null) {
             binding.book = book
         }
     }
 
-    companion object{
+    companion object {
         private const val EXTRA_BOOK = "book"
-        fun start(context: Context, book: Book){
-            Intent(context, BookDetailsActivity::class.java).apply {
-                putExtra(EXTRA_BOOK, Parcels.wrap(book))
-            }
+        fun start(context: Context, book: Book) {
+            context.startActivity(
+                Intent(context, BookDetailsActivity::class.java).apply {
+                    putExtra(EXTRA_BOOK, Parcels.wrap(book))
+                }
+            )
         }
     }
 
