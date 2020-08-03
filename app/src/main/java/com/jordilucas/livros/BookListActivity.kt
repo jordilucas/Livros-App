40c3ea6157d3c63093085c3a313d14a3b3e7549f
Jordi <jordilucas3@gmail.com>
2020-08-03 10:44:58 -0300

    binding adapter para imagens

--- app/src/main/java/com/jordilucas/livros/BookListActivity.kt
+++ app/src/main/java/com/jordilucas/livros/BookListActivity.kt
@@ -10,18 +10,19 @@ class BookListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        BookDetailsActivity.start(this, Book(
-            id="1",
-            title = "Dominando Android com Kotlin",
-            author="Nelson Glauber",
-            coverUrl = "https://s3.novatec.com.br/capas-ampliadas/capa-ampliada-9788575224632.jpg",
-            pages=954,
-            year=2018,
-            publisher = Publisher("1", "Novatec"),
-            available = true,
-            mediaType = MediaType.PAPER,
+        BookDetailsActivity.start(this, Book().apply {
+            id = "1"
+            title = "Dominando Android com Kotlin"
+            author = "Nelson Glauber"
+            coverUrl = "https://s3.novatec.com.br/capas-ampliadas/capa-ampliada-9788575224632.jpg"
+            pages = 954
+            year = 2018
+            publisher = Publisher("1", "Novatec")
+            available = true
+            mediaType = MediaType.PAPER
             rating = 5.0f
+        }
         )
-        )
+
     }
 }
