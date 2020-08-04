767b3c0590d92ce44c70e8fc58d8dce975319101
Jordi <jordilucas3@gmail.com>
2020-08-04 16:31:55 -0300

    logout

--- app/src/main/java/com/jordilucas/livros/BookListActivity.kt
+++ app/src/main/java/com/jordilucas/livros/BookListActivity.kt
@@ -2,8 +2,11 @@ package com.jordilucas.livros
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
 import com.jordilucas.livros.adapter.BookAdapter
 import com.jordilucas.livros.model.Book
 import com.jordilucas.livros.model.MediaType
@@ -14,6 +17,19 @@ class BookListActivity : BaseActivity() {
     /*override fun init() {
         TODO("Not yet implemented")
     }*/
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.book_list_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item?.itemId == R.menu.book_list_menu){
+            FirebaseAuth.getInstance().signOut()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
